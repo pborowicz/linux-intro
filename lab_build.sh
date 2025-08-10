ff4df17fef8c438d767a8dea2ccd53f173823189
Paul Borowicz <30293004+pborowicz@users.noreply.github.com>
2025-08-10 18:17:05 -0400

    Update lab_build.sh

--- lab_build.sh
+++ lab_build.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 #install git
-zypper in git
+#zypper in git
+#microOS
+sudo transaction-update pkg install git
+sudo transactional-updata apply
 
 # get matrix script
 mkdir ~/git; cd ~/git
